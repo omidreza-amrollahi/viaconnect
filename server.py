2d27bc81c28b7485b470ee3594c8d0d1a38f265c
omidreza-amrollahi <72734549+omidreza-amrollahi@users.noreply.github.com>
2022-05-11 08:47:59 +0200

    second version

--- server.py
+++ server.py
@@ -5,15 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('./index.html')
-
-
+    data_carrier = pd.read_csv('database_carrier.txt', sep=',')
+    return render_template('./index.html', tables=[data_carrier.to_html()], titles=[''])
 
 @app.route("/<string:page_name>")
 def html_page(page_name):
-    data = pd.read_csv('database.txt', sep=',')
-    with open("database.csv") as file:
-        return render_template(page_name, tables=[data.to_html()], titles=[''])
+    data_carrier = pd.read_csv('database_carrier.txt', sep=',')
+    data_sender = pd.read_csv('database_sender.txt', sep=',')
+    return render_template(page_name, tables=[data_carrier.to_html()], titles=[''])
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
@@ -28,16 +27,8 @@ def submit_form():
         return 'something wrong!'
 
 def write_to_file(data):
-    with open('database.txt', mode='a') as database:
+    with open('database_carrier.txt', mode='a') as database:
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
         file = database.write(f'\n{email},{subject},{message}')
-
-def write_to_csv(data):
-    with open('database.csv', newline='', mode='a') as database2:
-        email = data["email"]
-        subject = data["subject"]
-        message = data["message"]
-        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
-        csv_writer.writerow([email, subject, message])
